0acf06cbf951ac7713415959034e638c7e55eb0a
paulthanksgiving <paulthanksgiving2@gmail.com>
2025-12-03 21:44:42 +0100

    Spring changes

--- niit-ecommerce-api/src/main/java/com/example/Niit_ecommerce_API/security/AuthenticationFilter.java
+++ niit-ecommerce-api/src/main/java/com/example/Niit_ecommerce_API/security/AuthenticationFilter.java
@@ -1,4 +1,59 @@
 package com.example.Niit_ecommerce_API.security;
 
-public class AuthenticationFilter {
+import com.example.Niit_ecommerce_API.model.request.LoginRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationFilter.class);
+
+    public AuthenticationFilter(AuthenticationManager manager) {
+        super(manager);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        LOGGER.info("Login Successful");
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        LOGGER.info("Login Unsuccessful");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try{
+            LoginRequest loginRequest = objectMapper.readValue(
+                    request.getInputStream(),
+                    LoginRequest.class
+            );
+
+            return getAuthenticationManager()
+                    .authenticate(
+                            new UsernamePasswordAuthenticationToken(
+                                    loginRequest.getEmail(),
+                                    loginRequest.getPassword()
+                            )
+                    );
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+    }
 }
--- niit-ecommerce-api/src/main/java/com/example/Niit_ecommerce_API/security/WebSecurity.java
+++ niit-ecommerce-api/src/main/java/com/example/Niit_ecommerce_API/security/WebSecurity.java
@@ -1,4 +1,34 @@
 package com.example.Niit_ecommerce_API.security;
 
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class WebSecurity {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManager manager = http
+                .getSharedObject(AuthenticationManagerBuilder.class)
+                .build();
+
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(manager);
+
+        return http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/login").permitAll()
+                )
+                .addFilter(authenticationFilter)
+                .authenticationManager(manager)
+                .build();
+    }
 }
