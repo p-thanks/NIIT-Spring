b66e642d44203b09cc62394bec76bcbe6541c29f
paulthanksgiving <paulthanksgiving2@gmail.com>
2025-12-08 20:50:34 +0100

    new java update

--- niit-ecommerce-api/src/main/java/com/example/Niit_ecommerce_API/security/AuthenticationFilter.java
+++ niit-ecommerce-api/src/main/java/com/example/Niit_ecommerce_API/security/AuthenticationFilter.java
@@ -2,6 +2,8 @@ package com.example.Niit_ecommerce_API.security;
 
 import com.example.Niit_ecommerce_API.model.request.LoginRequest;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,7 +16,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -26,6 +32,25 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        Instant now = Instant.now();
+        now.plusSeconds(60 * 10);
+
+        String key = "sbcabcuygwbivewkwihqwwnihnfownqofwqoiefjwuoffwiufofqwdf";
+        byte[] keyBytes = key.getBytes();
+        SecretKey secretKey = new SecretKeySpec(keyBytes, SignatureAlgorithm.HS512.getJcaName());
+
+        Jwts
+                .builder()
+                .issuedAt(new Date())
+                .expiration(new Date(now.toEpochMilli()))
+                .subject("username")
+                .signWith(secretKey)
+                 .compact();
+
+        response.addHeader("Authorization", "Bearer token...");
+
+        chain.doFilter(request, response);
+
         LOGGER.info("Login Successful");
     }
 
--- niit-ecommerce-api/src/main/java/com/example/Niit_ecommerce_API/security/WebSecurity.java
+++ niit-ecommerce-api/src/main/java/com/example/Niit_ecommerce_API/security/WebSecurity.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -29,6 +30,9 @@ public class WebSecurity {
                 )
                 .addFilter(authenticationFilter)
                 .authenticationManager(manager)
-                .build();
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(cors -> cors.disable())
+                .csrf(csrf -> csrf.disable())
+                .getOrBuild();
     }
-}
+  }
